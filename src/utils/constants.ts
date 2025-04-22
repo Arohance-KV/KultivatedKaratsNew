f26a2005c409fd571b6b6755ffe6cf9a53b460f3
Aaaryan-R <87854481+Aaaryan-R@users.noreply.github.com>
2025-04-22 14:40:33 +0530

    Update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,7 +8,7 @@ export const INSTAGRAM = `https://www.instagram.com/kultivatedkarats/`;
 export const FACEBOOK = `https://www.facebook.com/61555613443273`;
 export const LINKEDIN = `https://www.linkedin.com/company/kultivated-karats/`;
 export const WHATSAPP = `https://wa.link/9y955q`;
-export const YOUTUBE = `https://wa.link/9y955q`;
+export const YOUTUBE = `https://www.youtube.com/@Kultivated-Karats/`;
 
 export const BLOGDATA = [
     {
