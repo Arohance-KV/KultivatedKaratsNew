b8279eb8b873eb81e045e40ab32b7f56709dd63e
Kunal Kumar Amar <159049370+kunalkumaramar@users.noreply.github.com>
2025-08-19 18:59:58 +0530

    Commented out the DashboardHomePage.tsx

--- src/components/adminPanel/DashboardHomePage.tsx
+++ src/components/adminPanel/DashboardHomePage.tsx
@@ -1,7 +1,7 @@
 export const DashboardHomePage = () => {
-    return (
-        <section className="bg-red-600/30 w-full h-full flex-1">
+    //return (
+        {/*<section className="bg-red-600/30 w-full h-full flex-1">
             Google analytics
-        </section>
-    );
+        </section>*/}
+    //);
 }
